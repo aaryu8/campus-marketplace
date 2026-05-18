716d73d35010da19d08d9756452256c2410d0464
Aryan Kaushik <aryan8kaushik@gmail.com>
2026-05-18 10:38:48 +0000

    Changed hosting service for backend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://campus-marketplace-production-c93f.up.railway.app/api/:path*',
+        destination: 'https://dormdeal.onrender.com/api/:path*',
       },
     ];
   },
